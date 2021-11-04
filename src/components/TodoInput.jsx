1bf242e637bd3937a516c0692f2d1ad7405f0c7c
MirzaQusyairi <mirza4909@gmail.com>
2021-11-05 00:20:09 +0700

    reacthook task

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import styles from './Styles.module.css'
+
+export default function TodoInput(props) {
+  const { addTodo } = props;
+  const [state, setState] = useState({
+    title: "",
+    completed: false,
+  });
+
+  const onChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isEmpty = state.title === "";
+    if (!isEmpty) {
+      const newData = {
+        title: state.title,
+        completed: state.completed,
+      };
+      addTodo(newData);
+      setState({
+        title: "",
+        completed: false,
+      });
+    } else {
+      alert("Title can't be empty");
+    }
+  };
+
+  return (
+    <div className={styles.TodoInputContainer}>
+      <input
+        type="text"
+        placeholder="Add todo..."
+        name="title"
+        className={styles.TodoInputTextBox}
+        value={state.title}
+        onChange={onChange}
+      />
+      <button onClick={handleSubmit} className={styles.TodoInputButton}>
+        Submit
+      </button>
+    </div>
+  );
+}
