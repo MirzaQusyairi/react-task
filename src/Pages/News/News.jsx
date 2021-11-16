197ad46cff83812f48bc39a43e942bc8c94d6412
MirzaQusyairi <mirza4909@gmail.com>
2021-11-16 17:55:24 +0700

    update news api with local api news

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -4,7 +4,8 @@ import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
 
 function News() {
-    const URL = "https://api.thenewsapi.com/v1/news/top?api_token=5wGlgJmd3lz24x83eZJb8gPWAElDbPl3W3sNubeW"
+    // const URL = "https://api.thenewsapi.com/v1/news/top?api_token=5wGlgJmd3lz24x83eZJb8gPWAElDbPl3W3sNubeW"
+    const URL = "https://newsapi.org/v2/top-headlines?country=id&apiKey=aa9d267d836649b69cbe650b35cc8764"
 
     const [news, setNews] = useState([]);
     const [err, setError] = useState("");
@@ -16,7 +17,7 @@ function News() {
             let result;
             try {
                 result = await axios.get(URL, { cancelToken: source.token });
-                setNews(result.data.data);
+                setNews(result.data.articles);
             } catch (error) {
                 setError(error);
             }
@@ -39,7 +40,7 @@ function News() {
                         <div className="col-md-4 mb-5">
                             <Card key={uuidv4()}
                                 title={item.title}
-                                content={item.snippet}
+                                content={item.description}
                                 url={item.url}
                                 image={item.image_url} />
                         </div>
