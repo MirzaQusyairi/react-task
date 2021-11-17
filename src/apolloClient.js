f1e4f3ae896bfb8456a830a487c0bf499b3bc5d9
MirzaQusyairi <mirza4909@gmail.com>
2021-11-17 12:12:46 +0700

    hide secret key

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import Key from './HasuraKey'
 
 const client = new ApolloClient({
   uri: 'https://mirza-q.hasura.app/v1/graphql',
   cache: new InMemoryCache(),
   headers: {
-    'x-hasura-admin-secret': 'G9jXvAVJcNOVltoie6EL7P0pSxY88Ux5MDzcBDPH0THR9myH9eQjadzSG5v4hmi6'
+    'x-hasura-admin-secret': Key
   },
 });
 
